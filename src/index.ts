d7b6d5610c166c3d9a625b6cc3aa75346a495378
samuel_ricardo <samuelricardoofficial@gmail.com>
2023-04-26 10:23:23 -0300

    :sparkles: setup: express

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
+import * as express from "express"
+import { Request, Response } from "express"
 import { Pedro } from "@/model"
 
 const message = "Hello World!"
 
 console.log(message, { PEDRO: new Pedro().pedro() })
+
+const app = express.application
+
+app.get("/", (req: Request, res: Response) => {
+  res.status(200).send({ PEDRO: ">:()" })
+})
+
+app.listen(3000, () => console.log("Listening on port 3000"))
